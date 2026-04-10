c8097d6e9edf34d6cdd5096a6f38ec924a3a1a83
Kingsleych7 <169157162+Kingsleych7@users.noreply.github.com>
2026-04-11 00:36:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
 // Parse USSD data (VERY IMPORTANT)
 app.use(express.urlencoded({ extended: false }));
 
@@ -12,7 +12,15 @@ const AfricasTalking = require("africastalking")({
     username: "sandbox",
     apiKey: "atsk_a281402b86f3cb842c15c96c41272554b8920fe5c3b260c14a02f46ee33b934e9f1c1de0"
 });
+mongoose.connect("mongodb+srv://Summitlink:<summit9876>@summitlinkcluster.t4qvdqt.mongodb.net/")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+const UserSchema = new mongoose.Schema({
+    phoneNumber: String,
+    balance: { type: Number, default: 1000 }
+});
 
+const User = mongoose.model("User", UserSchema);
 // =========================
 // TEST ROUTE (CHECK IF SERVER IS LIVE)
 // =========================
@@ -23,8 +31,8 @@ app.get("/", (req, res) => {
 // =========================
 // USSD ROUTE (MAIN MENU)
 // =========================
-app.post("/ussd", (req, res) => {
-    const { text = "" } = req.body;
+app.post("/ussd", async (req, res) => {
+    const { const { text = "", phoneNumber } = req.body;
 
     let response = "";
 
@@ -38,7 +46,13 @@ app.post("/ussd", (req, res) => {
         response = "CON My Account\n1. Check Balance\n2. Wallet Info";
     }
     else if (text === "1*1") {
-        response = "END Your balance is ₦500";
+    const user = await User.findOne({ phoneNumber });
+
+    if (!user) {
+        response = "END User not found";
+    } else {
+        response = `END Your balance is ₦${user.balance}`;
+}
     }
     else if (text === "1*2") {
         response = "END Wallet active";
