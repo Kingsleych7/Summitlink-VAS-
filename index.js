48cc9775e3f752d7c29e44ff5a9b3d29861b63d3
Kingsley <you@example.com>
2026-04-27 03:07:47 +0100

    USSD navigation update

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const axios = require("axios");
+const crypto = require("crypto");
+
+const redis = require("./src/config/redis");
+const PORT = process.env.PORT || 10000;
+
+const { getRequestId } = require("./src/utils/idempotency");
+
+const airtimeQueue = require("./src/services/queue");
+
+const sendSMS = require("./src/services/sms");
+
 const ussdController = require("./src/ussd/ussdController");
 
 const app = express();
@@ -13,14 +25,23 @@ app.use(express.json());
 // USSD route
 app.post("/ussd", ussdController);
 
-// DB
+// =======================
+// MOCK FUNCTIONS
+// =======================}
+
+async function buyData(phone, plan) {
+    return true;
+}
+
+// =======================
+// MONGODB
+// =======================
+>>>>>>> 431bdfd (USSD navigation update)
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("DB connected"))
 .catch(err => console.log("DB error:", err));
 
-// SERVER
-const PORT = process.env.PORT || 10000;
-
+// Start server
 app.listen(PORT, () => {
     console.log("Server running on port", PORT);
 });
